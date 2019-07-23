0e9a1c76d37e6bfa454bffa4feb2537b1c5116c9
Cphayim <i@cphayim.me>
2019-07-23 21:36:01 +0800

    opt: 优化自动发布和自动部署脚本

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -2,7 +2,7 @@
 /*
  * @Author: Cphayim
  * @Date: 2019-06-28 09:21:54
- * @LastEditTime: 2019-07-23 21:08:48
+ * @LastEditTime: 2019-07-23 21:22:58
  * @Description: 一键部署脚本
  */
 import sh from 'shelljs'
@@ -16,27 +16,40 @@ import {
   SERVER_SIDE_NGINX_CONF_DIR,
   NGINX_CONF
 } from './config'
+import { Logger } from './log'
 
 /**
  * ?将模板包和配置文件上传到服务器对应目录
  * ?上传 nginx 配置文件，重启 nginx 服务
  */
-sh.exec(
+Logger.info('开始执行部署...')
+const { code, stderr } = sh.exec(
   `
-  ssh ${SERVER_SIDE_USER}@${SERVER_SIDE_IP} -i ${PRIVATE_KEY} "
-    # 创建对应目录，设置权限
-    mkdir -p ${SERVER_SIDE_RELEASE_DIR}
-    chmod 755 ${SERVER_SIDE_RELEASE_DIR}
-  "
-  # 将 release 目录下的文件递归提交到服务端指定目录
-  scp -i ${PRIVATE_KEY} -r ${RELEASE_DIR} ${YML_FILE} ${SERVER_SIDE_USER}@${SERVER_SIDE_IP}:${SERVER_SIDE_RELEASE_DIR}
-  # 上传 nginx 配置
-  scp -i ${PRIVATE_KEY} ${NGINX_CONF} ${SERVER_SIDE_USER}@${SERVER_SIDE_IP}:${SERVER_SIDE_NGINX_CONF_DIR}
+    set -e
 
-  ssh ${SERVER_SIDE_USER}@${SERVER_SIDE_IP} -i ${PRIVATE_KEY} "
-    # 重载 nginx 服务
-    systemctl reload nginx
-  "
-`,
+    ssh ${SERVER_SIDE_USER}@${SERVER_SIDE_IP} -i ${PRIVATE_KEY} "
+      set -e
+      # 创建对应目录，设置权限
+      mkdir -p ${SERVER_SIDE_RELEASE_DIR}
+      chmod 755 ${SERVER_SIDE_RELEASE_DIR}
+    "
+
+    # 将 release 目录下的文件递归提交到服务端指定目录
+    scp -i ${PRIVATE_KEY} -r ${RELEASE_DIR} ${YML_FILE} ${SERVER_SIDE_USER}@${SERVER_SIDE_IP}:${SERVER_SIDE_RELEASE_DIR}
+    # 上传 nginx 配置
+    scp -i ${PRIVATE_KEY} ${NGINX_CONF} ${SERVER_SIDE_USER}@${SERVER_SIDE_IP}:${SERVER_SIDE_NGINX_CONF_DIR}
+
+    ssh ${SERVER_SIDE_USER}@${SERVER_SIDE_IP} -i ${PRIVATE_KEY} "
+      set -e
+      # 重载 nginx 服务
+      systemctl reload nginx
+    "
+  `,
   { shell: '/bin/zsh' }
 )
+
+if (!code) {
+  Logger.success('部署成功')
+} else {
+  Logger.error(`部署失败: ${stderr}`)
+}
--- scripts/release.js
+++ scripts/release.js
@@ -1,43 +1,65 @@
+#!/usr/bin/env node
 /*
  * @Author: Cphayim
  * @Date: 2019-06-28 16:28:26
- * @LastEditTime: 2019-07-23 14:32:05
- * @Description: 生成发布文件和配置
+ * @LastEditTime: 2019-07-23 21:32:28
+ * @Description: 一键发布脚本
  */
 import { join } from 'path'
 import { writeFileSync } from 'fs'
 import sh from 'shelljs'
 import YAML from 'yaml'
 
-import { PKG_DIR, RELEASE_DIR, TGZ_EXT, releaseInfoTpl, ROOT_DIR } from './config'
+import { PKG_DIR, RELEASE_DIR, TGZ_EXT, releaseInfoTpl, ROOT_DIR, YML_FILE } from './config'
 import { Logger } from './log'
+
+/**
+ * ? 将 packages 下的所有目录，生成对应的 tgz 文件保存在 release 目录
+ * ? 在根目录生成 boilerplate.yml 描述文件
+ */
 
 // 初始化目录（如果存在则忽略）-> mkdir -p "$RELEASE_DIR"
 sh.mkdir('-p', RELEASE_DIR)
 // 清空目录 -> rm -f "$RELEASE_DIR/*"
 sh.rm('-f', join(RELEASE_DIR, '*'))
 
+const map = {}
+
 // 生成发布文件 tgz
 Logger.info('开始创建发布文件包...')
-const map = {}
-sh.ls(PKG_DIR).forEach(pkgName => {
+
+const result = sh.ls(PKG_DIR).every(pkgName => {
   // 找到对应 boilerplate 下的 package.json 读取版本号
   const version = require(join(PKG_DIR, pkgName, 'package.json')).version
   const tgzName = `${pkgName}${TGZ_EXT}`
   const output = join(RELEASE_DIR, tgzName)
-  Logger.info(`创建 tgz 文件 ${tgzName}`)
-  sh.exec(
+  Logger.info(`创建 tgz 文件 ${tgzName}...`)
+
+  const { code, stderr } = sh.exec(
     `
-    cd ${PKG_DIR}
-    # 创建 tgz 压缩包
-    tar --exclude=node_modules -P -cvzf ${output} ${pkgName}
+      set -e
+
+      cd ${PKG_DIR}
+      # 创建 tgz 压缩包
+      tar --exclude=node_modules -P -cvzf ${output} ${pkgName}
     `,
-    { silent: true }
+    { silent: true, shell: '/bin/zsh' }
   )
+  if (code) {
+    Logger.error(`${tgzName}打包失败: ${stderr}`)
+    return false
+  }
   Logger.success(`${output}`)
   map[pkgName] = { version, tgz: tgzName }
+  return true
 })
-Logger.success('发布文件包创建成功')
+
+if (result) {
+  Logger.success('发布文件包创建成功')
+  Logger.info('开始生成 yml 文件')
+  genReleaseYaml()
+  Logger.success(`写入文件: ${YML_FILE}`)
+}
 
 function genReleaseYaml() {
   releaseInfoTpl.forEach(base => {
@@ -48,6 +70,5 @@ function genReleaseYaml() {
       }
     })
   })
-  writeFileSync(join(ROOT_DIR, 'boilerplate.yml'), YAML.stringify(releaseInfoTpl))
+  writeFileSync(YML_FILE, YAML.stringify(releaseInfoTpl))
 }
-genReleaseYaml()
