7c9ad4a5a354ddc339cc93e9cb1904f9da12f5e3
Cphayim <i@cphayim.me>
2020-01-10 14:41:04 +0800

    feat: react 模板配置更新

--- packages/react-redux-antd-boilerplate/config-overrides.js
+++ packages/react-redux-antd-boilerplate/config-overrides.js
@@ -1,8 +1,17 @@
-const { override, fixBabelImports, addWebpackAlias } = require('customize-cra')
+/*
+ * @Author: Cphayim
+ * @Date: 2019-08-28 17:22:39
+ * @LastEditTime: 2019-11-15 13:44:38
+ * @Description: create-react-app 配置重写
+ */
+
 const path = require('path')
+const { override, fixBabelImports, addWebpackAlias } = require('customize-cra')
+
 function resolve(dir) {
   return path.join(__dirname, '.', dir)
 }
+
 module.exports = override(
   // 配置路径别名
   addWebpackAlias({
