2474789025d3627e7314292902c249f55e336cc9
Cphayim <i@cphayim.me>
2020-03-14 16:19:57 +0800

    feat: vant 模板添加部分基础组件

---
+++ packages/vue-elementui-boilerplate/src/config/index.js
@@ -0,0 +1,6 @@
+/*
+ * @Author: Cphayim
+ * @Date: 2020-03-14 16:17:42
+ * @LastEditTime: 2020-03-14 16:17:43
+ * @Description:
+ */
---
+++ packages/vue-vantui-boilerplate/src/components/common/tag-list.vue
@@ -0,0 +1,28 @@
+<!--
+ * @Author: Cphayim
+ * @Date: 2019-10-10 09:34:45
+ * @LastEditTime: 2019-10-10 14:21:04
+ * @Description: 标签列表
+ -->
+
+<template>
+  <div class="tag-list">
+    <slot />
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'TagList',
+}
+</script>
+
+<style lang="scss" scoped>
+.tag-list {
+  overflow: hidden;
+  display: flex;
+  flex-wrap: wrap;
+  padding: val(10) val(20);
+  background-color: #fff;
+}
+</style>
