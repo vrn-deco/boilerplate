d0783141710e0a185cf970f8a99e3d749a7cdf72
Cphayim <i@cphayim.me>
2020-04-08 17:19:55 +0800

    feat: vue 模板 apis, config 目录软链管理

--- packages/vue-vantui-boilerplate/vue.config.js
+++ packages/vue-vantui-boilerplate/vue.config.js
@@ -1,10 +1,22 @@
+const argv = require('yargs').argv
+
 const { resolve } = require('path')
 module.exports = {
   productionSourceMap: false,
   pluginOptions: {
     'style-resources-loader': {
       preProcessor: 'scss',
-      patterns: [resolve(__dirname, 'src/assets/scss/libs/*.scss')]
-    }
-  }
+      patterns: [resolve(__dirname, 'src/assets/scss/libs/*.scss')],
+    },
+  },
+  chainWebpack: config => {
+    config.plugin('define').tap(definitions => {
+      // process.env
+      definitions[0]['process.env'] = {
+        ...definitions[0]['process.env'],
+        CUSTOM_ENV: JSON.stringify(argv['custom-env']),
+      }
+      return definitions
+    })
+  },
 }
