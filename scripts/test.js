5d2277b8b94d68e36ccd9346e88d4d031decd605
Cphayim <i@cphayim.me>
2020-07-06 12:55:34 +0800

    chore(ci): init ci

---
+++ scripts/test.js
@@ -0,0 +1,10 @@
+/*
+ * @Author: Cphayim
+ * @Date: 2020-07-06 12:52:51
+ * @LastEditTime: 2020-07-06 12:54:53
+ * @Description: 测试流程
+ */
+
+import { Logger } from '@naughty/logger'
+
+Logger.info('OK')
