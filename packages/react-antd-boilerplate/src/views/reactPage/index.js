c121f7afe1f28e40a62e9387173d56f3baca9ae5
PC-20170929SHIH\Administrator <15090221166@163.com>
2020-07-12 12:44:04 +0800

    refactor(react-antd): 修改文件

--- packages/react-antd-boilerplate/src/views/reactPage/index.js
+++ packages/react-antd-boilerplate/src/views/reactPage/index.js
@@ -1,21 +1,21 @@
 /*
  * @Author: yugeStrive
  * @Date: 2020-07-12 10:22:30
- * @LastEditTime: 2020-07-12 12:31:00
+ * @LastEditTime: 2020-07-12 12:40:40
  * @Description: react-antd-boilerplate先导页
  */
 
 import React, { Component } from 'react'
-import logo from '@/logo.svg'
+// import logo from '@/logo.svg'
 import { Button } from 'antd'
-import '@/assets/scss/reactPage.scss'
+// import '@/assets/scss/reactPage.scss'
 
 class ReactPage extends Component {
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
+          {/* <img src={logo} className="App-logo" alt="logo" /> */}
           <p>
             <Button type="primary" size="large" shape="circle" className="button-transform">
               VRN
