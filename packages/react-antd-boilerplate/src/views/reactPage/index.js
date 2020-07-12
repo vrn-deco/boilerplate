8a1da9b546c80c16c4323067dd130d06de5f56b5
PC-20170929SHIH\Administrator <15090221166@163.com>
2020-07-12 12:32:36 +0800

    refactor(react-antd): 删除jsconfig.js文件

--- packages/react-antd-boilerplate/src/views/reactPage/index.js
+++ packages/react-antd-boilerplate/src/views/reactPage/index.js
@@ -1,7 +1,7 @@
 /*
  * @Author: yugeStrive
  * @Date: 2020-07-12 10:22:30
- * @LastEditTime: 2020-07-12 11:59:26
+ * @LastEditTime: 2020-07-12 12:31:00
  * @Description: react-antd-boilerplate先导页
  */
 
