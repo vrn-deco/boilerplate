b520c24c28d6c9237ba07a594895db680311075a
yugestrive <15090221166@163.com>
2020-08-11 16:59:53 +0800

    refactor(react-antd): 修复因store、routerView引入顺序，导致的死循环问题

--- packages/react-antd-boilerplate/src/App.jsx
+++ packages/react-antd-boilerplate/src/App.jsx
@@ -1,17 +1,19 @@
 /*
  * @Autor: yugeStrive
  * @Date: 2020-07-07 17:02:40
- * @LastEditTime: 2020-07-16 15:21:47
+ * @LastEditTime: 2020-08-11 16:47:57
  * @Description: 主页面
  */
 
 import React, { Component } from 'react'
 import { Provider } from 'react-redux'
 import { ConnectedRouter } from 'connected-react-router'
-import RouterView from '@/router'
-import { history } from '@/router/history'
-import routeList from './views/root.routes'
+
+// 引入store与router的前后顺序，保证在挂在dom时引用的仓库数据字段提前加载
 import store from '@/store'
+import { history } from '@/router/history'
+import RouterView from '@/router'
+import routeList from './views/root.routes'
 
 class App extends Component {
   render() {
@@ -24,6 +26,5 @@ class App extends Component {
     )
   }
 }
-
 
 export default App
