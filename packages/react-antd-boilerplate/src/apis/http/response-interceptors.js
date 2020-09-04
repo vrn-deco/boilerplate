5a989fa1632ba45367eaf91fb0aa80ed4cac43cf
yugestrive <15090221166@163.com>
2020-09-04 17:20:17 +0800

    refactor(react-antd): 处理response的code逻辑

--- packages/react-antd-boilerplate/src/apis/http/response-interceptors.js
+++ packages/react-antd-boilerplate/src/apis/http/response-interceptors.js
@@ -1,7 +1,7 @@
 /*
  * @Autor: yugeStrive
  * @Date: 2020-07-29 15:12:15
- * @LastEditTime: 2020-08-05 10:24:42
+ * @LastEditTime: 2020-09-04 17:17:42
  * @Description: 响应拦截处理
  */
 
@@ -14,10 +14,7 @@ export const registerResponseInterceptor = (Axios) => {
   Axios.interceptors.response.use((response) => {
     const res = response.data
     const code = res[service.RESPONSE_CODE_FILED]
-    if (code === service.RESPONSE_CODE.OK) {
-      // 成功，直接返回 data
-      return res[service.RESPONSE_DATA_FILED]
-    } else if (code === service.RESPONSE_CODE.UNAUTHORIZED) {
+    if (code === service.RESPONSE_CODE.UNAUTHORIZED) {
       // token 过期或未登录
       // 当 config.UNAUTHORIZED_REDIRECT_PATH 有设置时进行自动跳转到先导页
       if (service.UNAUTHORIZED_REDIRECT_PATH) {
@@ -28,7 +25,7 @@ export const registerResponseInterceptor = (Axios) => {
       throw new Error(res[service.RESPONSE_MESSAGE_FILED])
     } else {
       // 其它 code，抛出异常
-      throw new Error(res[service.RESPONSE_MESSAGE_FILED])
+      return res
     }
   })
 }
