e7fffbc9287936b3bcc1bfca8dc2d540e227ca91
Cphayim <i@cphayim.me>
2020-12-14 09:45:41 +0800

    feat(vue): 添加了一些通用装饰器和指令

--- packages/vue-elementui-boilerplate/src/utils/decorators.js
+++ packages/vue-elementui-boilerplate/src/utils/decorators.js
@@ -1,7 +1,7 @@
 /*
  * @Author: Cphayim
  * @Date: 2019-10-05 01:37:01
- * @LastEditTime: 2020-10-21 14:58:51
+ * @LastEditTime: 2020-12-14 09:17:58
  * @Description: 装饰器
  */
 import { Loading, Message } from 'element-ui'
@@ -34,6 +34,66 @@ export function UseLoading(message = '正在加载...') {
         })
       } finally {
         t.close()
+      }
+    }
+  }
+}
+
+/**
+ * 防抖，防止触发多次点击，造成资源浪费
+ * (在N秒内只能执行一次事件，若在N秒内再次进行触发，则会清除前一个事件，重新进行计算)
+ * @param {Number} delay
+ */
+export function Debounce(delay = 100) {
+  let timeOut
+  return (t, k, p) => {
+    const fn = p.value
+    p.value = function(...args) {
+      if (timeOut) clearTimeout(timeOut)
+      timeOut = setTimeout(() => {
+        fn.call(this, ...args)
+      }, delay)
+    }
+  }
+}
+
+/**
+ * 节流，防止触发多次点击，造成资源浪费
+ * (连续触发事件在N秒内只执行一次函数，会起到稀释效果)
+ * @param {Number} delay
+ */
+export function Throttle(delay = 200) {
+  let timeOut
+  return (t, k, p) => {
+    const fn = p.value
+    p.value = function(...args) {
+      if (!timeOut) {
+        timeOut = setTimeout(() => {
+          timeOut = null
+          fn.apply(this, args)
+        }, delay)
+      }
+    }
+  }
+}
+
+/**
+ * 锁
+ * @param
+ */
+export function Lock() {
+  return (t, k, p) => {
+    let locked = false
+    const fn = p.value
+    p.value = async function(...args) {
+      if (locked) return
+      try {
+        locked = true
+        await fn.call(this, ...args)
+      } catch (error) {
+        console.err(error)
+      } finally {
+        locked = false
       }
     }
   }
--- packages/vue-vantui-h5plus-boilerplate/src/directives/index.js
+++ packages/vue-vantui-h5plus-boilerplate/src/directives/index.js
@@ -1,10 +1,25 @@
 /*
  * @Author: Cphayim
  * @Date: 2019-09-29 13:59:27
- * @LastEditTime: 2019-09-29 14:00:31
+ * @LastEditTime: 2020-12-14 09:41:37
  * @Description: 指令入口
  */
 
 export const registerDirectives = Vue => {
   // 这里注册你的全局指令
+  // 防抖
+  // 实例v-debounce:[arg]="[fn, event, arg]"
+  Vue.directive('debounce', {
+    inserted: function(el, binding, vnode) {
+      let timer = null
+      let delay = binding.arg || 500
+      const [fn, event = 'click', ...arg] = binding.value
+      el.addEventListener(event, () => {
+        if (timer) clearTimeout(timer)
+        timer = setTimeout(() => {
+          fn(...arg)
+        }, delay)
+      })
+    },
+  })
 }
