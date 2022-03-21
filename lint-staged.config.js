ad9bf3c0c1a58fdd874037a03189ba8cea92e4ec
Cphayim <i@cphayim.me>
2022-03-22 06:51:36 +0800

    ci(actions): deploy only when merge into main

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -3,10 +3,12 @@ const micromatch = require('micromatch')
 module.exports = {
   '*.{js,json}': (files) => {
     const match = micromatch.not(files, ['**/boilerplate/**'], { dot: true })
-    return `prettier --write ${match.join(' ')}`
+    return !match.length ? [] : [`prettier --write ${match.join(' ')}`]
   },
   '*.ts': (files) => {
     const match = micromatch.not(files, ['**/boilerplate/**'], { dot: true })
-    return [`eslint ${match.join(' ')}`, `prettier --write --parser typescript ${match.join(' ')}`]
+    return !match.length
+      ? []
+      : [`eslint ${match.join(' ')}`, `prettier --write --parser typescript ${match.join(' ')}`]
   },
 }
