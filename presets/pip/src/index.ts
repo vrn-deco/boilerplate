5ae93f42bff5aefdfc642e2ae69c41e7f06db92f
Cphayim <i@cphayim.me>
2022-03-24 01:14:27 +0800

    feat(preset): add pip preset installer (#106)

---
+++ presets/pip/src/index.ts
@@ -0,0 +1,41 @@
+import execa from 'execa'
+
+import { logger } from '@ombro/logger'
+import type { PresetRunner } from '@vrn-deco/boilerplate-protocol'
+import { BaseRunner } from '@vrn-deco/boilerplate-preset-base'
+
+export class PIPRunner extends BaseRunner {
+  override async install(): Promise<boolean> {
+    // The custom script is invoked and exits
+    if (await super.install()) return true
+
+    // install dependencies
+    const { autoInstallDeps = false } = this.options
+    if (autoInstallDeps) {
+      await this.installDeps()
+    }
+
+    return false
+  }
+
+  async installDeps(): Promise<void> {
+    if (!this.cmdIsExists('pipenv')) {
+      logger.warn('pipenv is not installed. Skipping installing dependencies.')
+      return
+    }
+    try {
+      await execa('pipenv', ['install'], { cwd: this.targetDir, stdio: 'inherit' })
+    } catch (err) {
+      logger.warn("Failed to install dependencies. You'll need to install it yourself")
+    }
+  }
+}
+
+export const runner: PresetRunner = async (options) => {
+  await new PIPRunner(options).run()
+}
+
+// Direct run
+if (require.main === module) {
+  runner({})
+}
