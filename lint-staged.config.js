d1b97dbf6e7985ee28eb6e59e78293aa06b59cee
Cphayim <i@cphayim.me>
2022-03-23 16:05:31 +0800

    ci(script): update preset creation script (#104)

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -2,11 +2,11 @@ const micromatch = require('micromatch')
 
 module.exports = {
   '*.{js,json}': (files) => {
-    const match = micromatch.not(files, ['**/boilerplate/**'], { dot: true })
+    const match = micromatch.not(files, ['**/.tpl/**', '**/boilerplate/**'], { dot: true })
     return !match.length ? [] : [`prettier --write ${match.join(' ')}`]
   },
   '*.ts': (files) => {
-    const match = micromatch.not(files, ['**/boilerplate/**'], { dot: true })
+    const match = micromatch.not(files, ['**/.tpl/**', '**/boilerplate/**'], { dot: true })
     return !match.length
       ? []
       : [`eslint ${match.join(' ')}`, `prettier --write --parser typescript ${match.join(' ')}`]
