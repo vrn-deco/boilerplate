bb02b0c050fbaed76bf5cd1dad4b922b99e48576
Hoyoe <hoyoe@cphayim.me>
2022-05-13 06:15:32 +0800

    fix(boilerplate): config type

--- packages/typescript/vue3-varlet/boilerplate/src/config/app.ts
+++ packages/typescript/vue3-varlet/boilerplate/src/config/app.ts
@@ -11,13 +11,13 @@ export const APP = {
    * 基本配置
    */
   // 当前运行模式
-  MODE: import.meta.env.NODE_ENV,
+  MODE: import.meta.env.MODE,
   // 应用名称
-  APP_NAME: import.meta.env.VUE_APP_NAME,
+  APP_NAME: import.meta.env.VITE_APP_NAME,
   // 应用版本
   VERSION: pkg.version,
   // 客户端密钥（用于本地加密）
-  CSK: import.meta.env.VUE_APP_SECRET_CODE,
+  CSK: import.meta.env.VITE_SERVER_BASE_URL,
 
   /**
    * 路由配置
@@ -30,5 +30,5 @@ export const APP = {
   /**
    * 其它配置，例如第三方插件
    */
-  VCONSOLE_ENABLE: import.meta.env.NODE_ENV === 'development',
+  VCONSOLE_ENABLE: import.meta.env.DEV,
 }
